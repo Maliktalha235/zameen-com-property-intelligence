785a859df52ac06a6c0d2c86477f440bb099e744
Maliktalha235 <malicktalha095@gmail.com>
2026-04-27 16:20:09 +0500

    Fixed env variable names for Railway

--- database.py
+++ database.py
@@ -6,12 +6,11 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306)),
-        auth_plugin="mysql_native_password"
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
 
 def create_table():
